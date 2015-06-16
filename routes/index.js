163facd5e3ddde12ee66cddde3a295498f408b59
Kinnard <khockenhull@gmail.com>
2015-06-15 20:25:30 -0400

    New site: new nav, mission page, conference page, team page

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Black Bitcoiners' });
 });
 
+router.get('/team', function(req, res) {
+  res.render('team', { title: 'Black Bitcoiners — Team' });
+});
+
+router.get('/mission', function(req, res) {
+  res.render('mission', { title: 'Black Bitcoiners — Mission' });
+});
+router.get('/mailinglist', function(req, res) {
+  res.render('mailingList', { title: 'Black Bitcoiners — Mail' });
+});
 
 function setEnvVars (req, cb){
     shell.env['email']  = req.body.email;
